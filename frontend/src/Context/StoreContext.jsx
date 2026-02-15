08b59681a58617eefdf234451cb265b36bd9c1f1
Mohammed Allam <muhammed.allam2004@gmail.com>
2026-02-15 06:57:59 +0200

    update backend url

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"; 
+    const url = "https://food-delivery-app-production-4da8.up.railway.app"; 
     const [token, setToken] = useState("");
     const [food_list, setFood_list] = useState([]);
     const [fitness_list, setFitness_list] = useState([]); 
